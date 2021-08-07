bf467ebd456918b8d7c242829b4d5f538567728e
Petr Janik <peta.janik@email.cz>
2021-08-07 21:38:49 +0200

    Format code

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,8 +1,8 @@
 import Reactotron from 'reactotron-react-native';
-import {reactotronRedux} from 'reactotron-redux';
+import { reactotronRedux } from 'reactotron-redux';
 
 const reactotron = Reactotron.configure({name: 'React Native Demo'})
-  .use(reactotronRedux()) //  <- here i am!
-  .connect(); //Don't forget about me!
+    .use(reactotronRedux())
+    .connect();
 
 export default reactotron;
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,13 +1,13 @@
-import {createStore, combineReducers} from 'redux';
+import { createStore, combineReducers } from 'redux';
 import countReducer from '../reducers/countReducer';
 import Reactotron from '../ReactotronConfig';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({count: countReducer});
 const configureStore = () => {
-  return createStore(
-    rootReducer,
-    composeWithDevTools(Reactotron.createEnhancer()),
-  );
+    return createStore(
+        rootReducer,
+        composeWithDevTools(Reactotron.createEnhancer()),
+    );
 };
 export default configureStore;
